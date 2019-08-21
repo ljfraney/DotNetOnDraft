93576977d4472bfcaef32ff092e55ad2997565d6
Josh Franey <ljfraney@yahoo.com>
2019-08-20 23:34:23 -0400

    Added Vue.js and wired up playlist

--- Src/DotNetOnDraft/gulpfile.js
+++ Src/DotNetOnDraft/gulpfile.js
@@ -1,6 +1,7 @@
 ﻿/// <binding BeforeBuild='default' ProjectOpened='default' /> 
 const { src, dest, series, parallel, watch } = require('gulp');
 const uglify = require('gulp-uglify');
+const flatten = require('gulp-flatten');
 const pipeline = require('readable-stream').pipeline;
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
@@ -13,24 +14,33 @@ var fileWatchConfig = [
     'css/**/*.scss'
 ];
 
-watch(fileWatchConfig, {}, parallel(compress));
+//watch(fileWatchConfig, {}, parallel(compress));
 
 function copy(cb) {
     console.log('Copying bootstrap files...');
     src('node_modules/bootstrap/dist/**/*')
-        .pipe(dest('wwwroot/lib/bootstrap/dist'));
+        .pipe(dest('wwwroot/lib/bootstrap/dist/'));
 
     console.log('Copying jquery files...');
     src('node_modules/jquery/dist/**/*')
-        .pipe(dest('wwwroot/lib/jquery/dist'));
+        .pipe(dest('wwwroot/lib/jquery/dist/'));
 
     console.log('Copying popper.js files...');
     src('node_modules/popper.js/dist/**/*')
-        .pipe(dest('wwwroot/lib/popper.js/dist'));
+        .pipe(dest('wwwroot/lib/popper.js/dist/'));
 
     console.log('Copying jplayer files...');
     src('node_modules/jplayer/dist/**/*')
-        .pipe(dest('wwwroot/lib/jplayer/dist'));
+        .pipe(dest('wwwroot/lib/jplayer/dist/'));
+
+    console.log('Copying vue files...');
+    src('node_modules/vue/dist/**/*')
+        .pipe(dest('wwwroot/lib/vue/dist/'));
+
+    console.log('Copying moment files...');
+    src(['node_modules/moment/**/moment.js', '!node_modules/moment/src/**', 'node_modules/moment/**/moment.*.js'])
+        .pipe(flatten())
+        .pipe(dest('wwwroot/lib/moment/'));
 
     cb();
 }
